ab2a07a640e1ca5fe0d705e1b90685285cc7ea26
Marcus Richard <marcus.a.richard@gmail.com>
2020-07-20 15:47:40 -1000

    renamed Viewcontroller and added file path of contents

---
+++ StormViewer/StormViewerVC.swift
@@ -0,0 +1,30 @@
+//
+//  StormViewerVC.swift
+//  StormViewer
+//
+//  Created by Marcus Richard on 7/20/20.
+//  Copyright © 2020 Marcus Richard. All rights reserved.
+//
+
+import UIKit
+
+class StormViewerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
