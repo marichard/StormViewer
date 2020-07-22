82963b75454b68d009dc09b3540bfe2512183227
Marcus Richard <marcus.a.richard@gmail.com>
2020-07-22 11:12:09 -1000

    created detail view controller

---
+++ StormViewer/DetailVC.swift
@@ -0,0 +1,25 @@
+//
+//  DetailVC.swift
+//  StormViewer
+//
+//  Created by Marcus Richard on 7/22/20.
+//  Copyright © 2020 Marcus Richard. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+	@IBOutlet var imageView: UIImageView!
+	
+	var selectedImage: String?
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
--- StormViewer/StormViewerVC.swift
+++ StormViewer/StormViewerVC.swift
@@ -38,7 +38,4 @@ class StormViewerVC: UITableViewController {
 		return cell
 	}
 	
-	
-       
-
 }
